1d726283c7e215271bf913bdc75768f1662a3afd
shuweic <shuweic227@gmail.com>
2023-10-06 13:38:25 -0500

    fest: show google map on pages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
+//
+// const user = new User();
+// console.log(user);
+// console.log('Hi there!');
+//
+// const company: Company = new Company();
+// console.log(company);
 
-const user = new User();
-console.log(user);
-console.log('Hi there!');
 
-const company: Company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 2,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
