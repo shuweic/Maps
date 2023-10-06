50f1377a3a5e7469d50c72ade1775121a4c259a7
shuweic <shuweic227@gmail.com>
2023-10-05 21:30:00 -0500

    feat: export User and Company

---
+++ src/User.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+
+export class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.person.firstName();
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude(),
+        };
+    }
+}
+
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+console.log(user);
+console.log('Hi there!');
+
+const company: Company = new Company();
+console.log(company);
